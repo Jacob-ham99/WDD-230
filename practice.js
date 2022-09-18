4f210041d91500b7d5fe3e6dcaf50e8cd5e714dc
Jacob-ham99 <ham20039@byui.edu>
2022-09-17 21:30:58 -0600

    new js

--- practice.js
+++ practice.js
@@ -1,18 +1,8 @@
 let date = new Date();
 
-console.log(date);
-
 let year = date.getFullYear();
 
-console.log(year);
-
-document.querySelector('h1').textContent = year;
-
 let current_date = document.lastModified;
-
-console.log(current_date);
-
-document.querySelector('div').textContent = current_date;
 
 document.querySelector('.year').textContent = year;
 
